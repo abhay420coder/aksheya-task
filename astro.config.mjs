95aa2c0e63146430b4497effe2ce329a4defbdc2
Abhay Kumar <abhay.kumar@cordis.us>
2024-06-21 23:56:27 +0530

    - git updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import netlify from "@astrojs/netlify";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  site: 'https://abhay420coder.github.io',
+  base: 'my-repo',
 });
